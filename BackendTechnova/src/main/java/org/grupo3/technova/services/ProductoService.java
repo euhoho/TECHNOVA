9ec4ef05c7165b3423a6e3fb13dd4fa2c78a8ef6
Rayan-t-t <rayan.torres@alumfp.udit.es>
2026-02-23 11:50:56 +0100

    feat: Implemetar producto
    Se ha implementado el controller,

--- BackendTechnova/src/main/java/org/grupo3/technova/services/ProductoService.java
+++ BackendTechnova/src/main/java/org/grupo3/technova/services/ProductoService.java
@@ -1,4 +1,31 @@
 package org.grupo3.technova.services;
+import org.grupo3.technova.data.enums.EnumCategoria;
+import org.grupo3.technova.repository.ProductoRepository;
+import org.grupo3.technova.data.model.Producto;
+import org.springframework.stereotype.Service;
 
-public interface ProductoService {
+import java.util.List;
+@Service
+public class ProductoService {
+
+    private final ProductoRepository productoRepository;
+    public ProductoService(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+    public List<Producto> findAll(){
+        return productoRepository.findAll();
+    }
+
+    public List<Producto> listar() {
+        return productoRepository.findAll();
+    }
+    public List<Producto> findByCategoria(EnumCategoria categoria) {
+        return productoRepository.findByCategoria(categoria);
+    }
+    public void save(Producto producto) {
+      /*  if (producto.getPrecio().equals(999.99)) {
+            throw new IllegalArgumentException("Precio no puede ser igual(Por alguna razon)");
+        }*/
+        productoRepository.save(producto);
+    }
 }
