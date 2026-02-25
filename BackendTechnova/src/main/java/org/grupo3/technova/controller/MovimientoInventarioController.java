f37c989130fe7d7be11a072cdfdfdb221d11ef65
ShaghyAsghari <shaghayegh.asghari@alumfp.udit.es>
2026-02-25 11:20:46 +0100

    movimiento inventario

--- BackendTechnova/src/main/java/org/grupo3/technova/controller/MovimientoInventarioController.java
+++ BackendTechnova/src/main/java/org/grupo3/technova/controller/MovimientoInventarioController.java
@@ -1,4 +1,45 @@
 package org.grupo3.technova.controller;
 
+import org.grupo3.technova.data.model.MovimientoInventario;
+import org.grupo3.technova.repository.MovimientoInventarioRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import javax.sql.DataSource;
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/api/movimientoinventario")
 public class MovimientoInventarioController {
+
+    private final MovimientoInventarioRepository movimientoRepository;
+    public MovimientoInventarioController(MovimientoInventarioRepository movimientoRepository) {
+        this.movimientoRepository = movimientoRepository;
+    }
+    // GET: Obtener todos los movimientos
+    @GetMapping
+    public List<MovimientoInventario> getAllMovimientos() {
+        return movimientoRepository.findAll();
+    }
+
+
+    // GET: Obtener un movimiento por id
+    @GetMapping("/{id}")
+    public MovimientoInventario getMovimientoById(@PathVariable Long id) {
+        return movimientoRepository.findAll().stream()
+                .filter(m -> m.getId_movimiento().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+    // ===============================
+    @GetMapping("/producto/{idProducto}")
+    public List<MovimientoInventario> listarPorProducto(@PathVariable Long idProducto) {
+        return movimientoRepository.findByProductoId(idProducto);
+    }
+    // ===============================
+    @PostMapping
+    public void guardar(@RequestBody MovimientoInventario movimiento) {
+        movimientoRepository.save(movimiento);
+    }
 }
--- BackendTechnova/src/main/java/org/grupo3/technova/repository/MovimientoInventario.java
+++ BackendTechnova/src/main/java/org/grupo3/technova/repository/MovimientoInventario.java
@@ -1,4 +1,106 @@
 package org.grupo3.technova.repository;
 
-public interface MovimientoInventario {
+import org.grupo3.technova.data.model.MovimientoInventario;
+import org.grupo3.technova.data.model.Producto;
+import org.grupo3.technova.data.enums.EnumTipoMovimiento;
+import org.grupo3.technova.data.enums.EnumCategoria;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface MovimientoInventarioRepository {
+    DataSource getDataSource();
+
+    default void save (MovimientoInventario movimiento) {
+        String sql = "{CALL sp_movimiento_inventario(?, ?, ?, ?, ?)}";
+
+        try (Connection con = getDataSource().getConnection();
+             CallableStatement cs = con.prepareCall(sql)) {
+            cs.setLong(1, movimiento.getId_producto().getId_producto());
+            cs.setString(2, movimiento.getTipo_movimiento().name());
+            cs.setDate(3, movimiento.getFecha_movimiento());
+            cs.setInt(4, movimiento.getCantidad_movimiento());
+            cs.setString(5, movimiento.getMotivo_movimiento());
+
+            cs.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error guardando movimiento", e);
+        }
+    }
+    default List<MovimientoInventario> findAll() {
+        List<MovimientoInventario> movimientos = new ArrayList<>();
+        String sql = "{CALL sp_movimiento_inventario_listar()}";
+
+        try (Connection con = getDataSource().getConnection();
+             CallableStatement cs = con.prepareCall(sql);
+             ResultSet rs = cs.executeQuery()) {
+
+            while (rs.next()) {
+                Producto producto = new Producto(
+                        rs.getLong("id_producto"),
+                        rs.getString("sku"),
+                        rs.getString("nombre"),
+                        rs.getString("descripcion"),
+                        rs.getDouble("precio"),
+                        rs.getInt("stock"),
+                        EnumCategoria.valueOf(rs.getString("categoria")),
+                        rs.getString("imagen")
+                );
+                MovimientoInventario m = new MovimientoInventario(
+                        rs.getLong(MovimientoInventario.ID_MOVIMIENTO),
+                        producto,
+                        EnumTipoMovimiento.valueOf(rs.getString(MovimientoInventario.TIPO_MOVIMIENTO)),
+                        rs.getDate(MovimientoInventario.FECHA_MOVIMIENTO),
+                        rs.getInt(MovimientoInventario.CANTIDAD_MOVIMIENTO),
+                        rs.getString(MovimientoInventario.MOTIVO_MOVIMIENTO)
+                );
+                movimientos.add(m);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error consultando movimientos del inventario.", e);
+        }
+        return movimientos;
+    }
+
+    default List<MovimientoInventario> findByProductoId(Long idProducto)    {
+        List<MovimientoInventario> movimientos = new ArrayList<>();
+        String sql = "{CALL sp_movimiento_inventario_listar_id_producto(?)}";
+
+        try (Connection con = getDataSource().getConnection();
+             CallableStatement cs = con.prepareCall(sql)) {
+            cs.setLong(1, idProducto);
+
+            try (ResultSet rs = cs.executeQuery()){
+                while(rs.next()) {
+                    Producto producto = new Producto(
+                            rs.getLong("id_producto"),
+                            rs.getString("sku"),
+                            rs.getString("nombre"),
+                            rs.getString("descripcion"),
+                            rs.getDouble("precio"),
+                            rs.getInt("stock"),
+                            EnumCategoria.valueOf(rs.getString("categoria")),
+                            rs.getString("imagen")
+                    );
+                    MovimientoInventario m = new MovimientoInventario(
+                            rs.getLong(MovimientoInventario.ID_MOVIMIENTO),
+                            producto,
+                            EnumTipoMovimiento.valueOf(rs.getString(MovimientoInventario.TIPO_MOVIMIENTO)),
+                            rs.getDate(MovimientoInventario.FECHA_MOVIMIENTO),
+                            rs.getInt(MovimientoInventario.CANTIDAD_MOVIMIENTO),
+                            rs.getString(MovimientoInventario.MOTIVO_MOVIMIENTO)
+                    );
+
+                    movimientos.add(m);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error consultando movimientos por producto", e);
+        }
+
+        return movimientos;
+    }
 }
